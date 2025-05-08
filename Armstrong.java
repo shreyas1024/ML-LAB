48ced04b2e02e5b58b39fed96367cbe156efe66a
shreyas1024 <shreyassahare2004@gmail.com>
2025-05-09 00:55:02 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,18 @@
+import java.util*;
+
+public class Armstrong{
+    int num;
+    int sum=0;
+
+    public static void main(String[]args){
+        System.out.println("enter a number");
+        Scanner sc = new Scanner(system.in);
+
+        num=sc.nextInt();
+ 
+
+
+
+    }
+
+}
